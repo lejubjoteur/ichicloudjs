44c0ef11d3e9b878f136c70e4dcc6351f41c565b
Quentin GIMENEZ <qgimenez@e2r11p14.clusters.42paris.fr>
2022-01-14 17:47:52 +0100

    code opti TODO opti strategy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function getCandles() {
 
 async function getBacktestDB() {
 	let db = [];
-	let timestamp = Date.parse('27 oct 2021');
+	let timestamp = Date.parse('01 nov 2021');
 	for (let i = 0; i < 20; i++) {
 		let response = await client.klines('BTCUSDT', '5m', {startTime: timestamp, limit: 1000})
 		db.push(...response.data)
@@ -41,165 +41,50 @@ async function getAverageInterval(candles, index, interval) {
 	return average;
 }
 
-// Highest and Lowest price since 9periods in past
-async function getConversionLine(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 1; i <= 9; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let conversionLine = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return conversionLine
-}
-
-// Highest and Lowest price since 26periods in the past
-async function getBaseLine(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 1; i <= 26; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let baseLine = ((parseFloat(hight) + parseFloat(low)) / 2)
-	return baseLine
-}
-
-// Highest and Lowest price since 52periods in the past ploted 26periods in the future
-async function getLeadingSpanB(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 1; i <= 52; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let leadingSpanB = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return leadingSpanB
-}
-
-// Closed price plotted 26 periods in the past
-async function getLaggingSpan(candles) {
-	let laggingSpan = parseFloat(candles[candles.length - 1][4])
-	return laggingSpan
-}
-
 // Last price (not necessarily closed)
 async function getLastPrice(candles) {
 	let lastPrice = parseFloat(candles[candles.length - 1][4])
 	return lastPrice
 }
 
-// --------------- New values needed ---------------
+// --------------- new Order ---------------
 
-async function getConversionLine52(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 52; i <= 52 + 9; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let conversionLine = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return conversionLine
-}
+let orders = [];
 
-async function getBaseLine52(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 52; i <= 52 + 26; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let baseLine = ((parseFloat(hight) + parseFloat(low)) / 2)
-	return baseLine
-}
-
-// Value of SpanB 52 periods in the past to compare with laggingSpan
-async function getOld52SpanB(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 52; i <= 52 + 52; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let leadingSpanB = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return leadingSpanB
-}
-
-async function getConversionLine26(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 26; i <= 26 + 9; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let conversionLine = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return conversionLine
-}
-
-async function getBaseLine26(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 26; i <= 26 + 26; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let baseLine = ((parseFloat(hight) + parseFloat(low)) / 2)
-	return baseLine
-}
-
-// Value of SpanB 26 periods in the past to compare with lastPrice
-async function getOld26SpanB(candles) {
-	let hight = 0;
-	let low = -1;
-	for (let i = 26; i <= 52 + 26; i++) {
-		if (hight < candles[candles.length - i][2])
-			hight = candles[candles.length - i][2]
-		if (low == -1 || low > candles[candles.length - i][3])
-			low = candles[candles.length - i][3]
-	}
-	let leadingSpanB = ((parseFloat(hight) + parseFloat(low)) / 2);
-	return leadingSpanB
+async function newOrder(candles, lastPrice, stopLoss, typePosition) {
+	let takeProfit = (lastPrice - stopLoss) * 100 / lastPrice * 1.5 * lastPrice / 100 + lastPrice
+	let order = new Order(candles[candles.length - 1][0], lastPrice, stopLoss, takeProfit, typePosition);
+	orders.push(order);
+	console.log(new Date(orders.slice(-1)[0].date).toString());
+	console.log("Price Order :", orders.slice(-1)[0].priceOrder);
+	console.log("Take profit :", orders.slice(-1)[0].takeProfit);
+	console.log("Stop loss :", orders.slice(-1)[0].stopLoss);
+	return true
 }
 
 // --------------- Ichimoku Indicator ---------------
 
 // All that we need for ichimoku strategy
 async function ichimokuCloud(candles) {
-	let conversionLine = await getConversionLine(candles);
-	let baseLine = await getBaseLine(candles);
+	let conversionLine = await getAverageInterval(candles, 1, 9);
+	let baseLine = await getAverageInterval(candles, 1, 26);
 	// console.log("baseLine :" + "\x1b[33m", baseLine,'\x1b[0m', "conversionLine :" + "\x1b[34m", conversionLine, '\x1b[0m')
 
 	let leadingSpanA = ((parseFloat(conversionLine) + parseFloat(baseLine)) / 2);
-	let leadingSpanB = await getLeadingSpanB(candles);
+	let leadingSpanB = await getAverageInterval(candles, 1, 52);
 	// console.log("leadingSpanA :" + "\x1b[32m", leadingSpanA,'\x1b[0m', "leadingSpanB :" + "\x1b[31m", leadingSpanB, '\x1b[0m')
 
-	let conversionLine52 = await getConversionLine52(candles);
-	let baseLine52 = await getBaseLine52(candles);
+	let conversionLine52 = await getAverageInterval(candles, 52, 52 + 9);
+	let baseLine52 = await getAverageInterval(candles, 52, 52 + 26);
 	let old52SpanA = ((parseFloat(conversionLine52) + parseFloat(baseLine52)) / 2);
-	let old52SpanB =  await getOld52SpanB(candles);
-	let laggingSpan = await getLaggingSpan(candles);
+	let old52SpanB =  await getAverageInterval(candles, 52, 52 + 52);
+	let laggingSpan = await getLastPrice(candles);
 	// console.log("laggingSpan :" + "\x1b[35m", laggingSpan,'\x1b[0m', "old52SpanA :" + "\x1b[32m", old52SpanA, '\x1b[0m', "old52SpanB :" + "\x1b[31m", old52SpanB, '\x1b[0m')
 
-	let conversionLine26 = await getConversionLine26(candles);
-	let baseLine26 = await getBaseLine26(candles);
+	let conversionLine26 = await getAverageInterval(candles, 26, 26 + 9);
+	let baseLine26 = await getAverageInterval(candles, 26, 26 + 26);
 	let old26SpanA = ((parseFloat(conversionLine26) + parseFloat(baseLine26)) / 2);
-	let old26SpanB = await getOld26SpanB(candles);
+	let old26SpanB = await getAverageInterval(candles, 26, 52 + 26);
 	let lastPrice = await getLastPrice(candles);
 	// console.log("lastPrice :" + "\x1b[36m", lastPrice,'\x1b[0m', "old26SpanA :" + "\x1b[32m", old26SpanA, '\x1b[0m', "old26SpanB :" + "\x1b[31m", old26SpanB, '\x1b[0m')
 
@@ -209,29 +94,23 @@ async function ichimokuCloud(candles) {
 		&& leadingSpanA > leadingSpanB
 		&& lastPrice > old26SpanA && lastPrice > old26SpanB
 		&& laggingSpan > old52SpanA && laggingSpan > old52SpanB
-		&& (!lastOrder || lastOrder.long == false))
-		{
+		&& (!lastOrder || lastOrder.long == false)
+		&& lastPrice > parseFloat(candles[candles.length - 2][4])
+	) {
 			console.log("\x1b[32m" + "Buy! Buy! Buy!", '\x1b[0m')
 			let stopLoss = old26SpanB;
-			let takeProfit = (lastPrice - stopLoss) * 100 / lastPrice * 1.5 * lastPrice / 100 + lastPrice
-			let order = new Order(candles[candles.length - 1][0], lastPrice, stopLoss, takeProfit, true);
-			orders.push(order);
-			console.log(new Date(orders.slice(-1)[0].date).toString());
-			return true;
+			return newOrder(candles, lastPrice, stopLoss, true);
 		}
 	else if (conversionLine < baseLine
 		&& leadingSpanA < leadingSpanB
 		&& lastPrice < old26SpanA && lastPrice < old26SpanB
 		&& laggingSpan < old52SpanA && laggingSpan < old52SpanB
-		&& (!lastOrder || lastOrder.long == true))
-		{
+		&& (!lastOrder || lastOrder.long == true)
+		&& lastPrice < parseFloat(candles[candles.length - 2][4])
+	) {
 			console.log("\x1b[31m" + "Sell! Sell! Sell!", '\x1b[0m')
 			let stopLoss = old26SpanA;
-			let takeProfit = (lastPrice - stopLoss) * 100 / lastPrice * 1.5 * lastPrice / 100 + lastPrice
-			let order = new Order(candles[candles.length - 1][0], lastPrice, stopLoss, takeProfit, false);
-			orders.push(order);
-			console.log(new Date(orders.slice(-1)[0].date).toString());
-			return true;
+			return newOrder(candles, lastPrice, stopLoss, false);
 		}
 	else
 		return false;
@@ -239,6 +118,7 @@ async function ichimokuCloud(candles) {
 
 class Order {
 	profit = 0
+	leverage = 1
 
 	constructor(date, priceOrder, stopLoss, takeProfit, long) {
 		this.date = date;
@@ -252,6 +132,7 @@ class Order {
 		this.profit = (this.priceOrder - this.stopLoss) * 100 / this.priceOrder;
 		if (this.long)
 			this.profit = -this.profit
+		this.profit = this.profit * this.leverage
 		console.log("bad")
 	}
 
@@ -259,11 +140,10 @@ class Order {
 		this.profit = (this.priceOrder - this.takeProfit) * 100 / this.priceOrder;
 		if (this.long)
 			this.profit = -this.profit
+		this.profit = this.profit * this.leverage
 		console.log("good")
 	}
 }
-
-let orders = [];
 
 async function main() {
 	// let candles = await getCandles()
@@ -280,25 +160,31 @@ async function main() {
 			let lastOrder = orders.slice(-1)[0]
 			let lastCandle = candles.slice(-1)[0]
 			position = false;
-			if (lastOrder.long && lastCandle[4] >= lastOrder.takeProfit)
+			if (lastOrder.long && lastCandle[2] >= lastOrder.takeProfit)
 				lastOrder.goodTrade();
-			else if (lastOrder.long && lastCandle[4] <= lastOrder.stopLoss)
+			else if (lastOrder.long && lastCandle[3] <= lastOrder.stopLoss)
 				lastOrder.badTrade();
-			else if (!lastOrder.long && lastCandle[4] <= lastOrder.takeProfit)
+			else if (!lastOrder.long && lastCandle[3] <= lastOrder.takeProfit)
 				lastOrder.goodTrade();
-			else if (!lastOrder.long && lastCandle[4] >= lastOrder.stopLoss)
+			else if (!lastOrder.long && lastCandle[2] >= lastOrder.stopLoss)
 				lastOrder.badTrade();
 			else
 				position = true;
 		}
 		db.shift()
 	}
-		console.log(orders.reduce((a, b) => {
+		console.log("Trades Win : " + orders.reduce((a, b) => {
 			if (b.profit > 0)
 				return a + 1
 			else
-				return a - 1
+				return a
 		}, 0))
+		console.log("Orders : " + orders.length)
+		let profits = 0;
+		for (let order of orders) {
+			profits = parseFloat(profits) + parseFloat(order.profit)
+		}
+		console.log("Profits : " + profits.toFixed(2) + "%")
 }
 
 main();
